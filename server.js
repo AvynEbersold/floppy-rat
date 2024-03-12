c05968312c1cd51fe7be25be5f6054eb7fc9219f
AVYN EBERSOLD <1793298-AVYNEBERSOLD@users.noreply.replit.com>
2024-03-12 14:29:11 +0000

    Use port assigned by heroku

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 app.use(express.static('public'));
 
-const port = 80;
+const port = process.env.PORT || 80;
 
 app.get("/", (req, res)=>{
 	console.log("requested");

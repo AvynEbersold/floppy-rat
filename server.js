0fcbe21c64a2173410fa5be2ff4c579ddf36fa4b
AVYN EBERSOLD <1793298-AVYNEBERSOLD@users.noreply.replit.com>
2024-03-13 16:22:28 +0000

    Add middleware to redirect from https to http

--- server.js
+++ server.js
@@ -4,6 +4,13 @@ app.use(express.static('public'));
 
 const port = process.env.PORT || 80;
 
+app.use(function(req, res, next) {
+	if (process.env.NODE_ENV != 'development' && req.secure) {
+	   return res.redirect("http://" + req.headers.host + req.url);
+	}
+	next();
+})
+
 app.get("/", (req, res)=>{
 	console.log("requested");
 	res.sendFile(__dirname + "/public/index.html");

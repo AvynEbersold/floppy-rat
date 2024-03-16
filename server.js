b253fc759cb1546ba39756f232935b7256114560
Avyn Ebersold <avyn.ebersold@gmail.com>
2024-03-16 02:53:30 +0000

    Comment it all back, HELP

--- server.js
+++ server.js
@@ -2,11 +2,15 @@ var express = require("express");
 const mongoose = require('mongoose');
 const session = require('express-session');
 const passport = require('passport');
-const passportLocalMongoose = require('passport-local-mongoose');
 
 const app = express();
 app.use(express.static("public"));
-app.enable('trust proxy')
+app.enable('trust proxy');
+
+var options = {
+	key: fs.readFileSync("privatekey.pem", "utf8"),
+	cert: fs.readFileSync("floppyrat_com.crt", "utf8"),
+};
 
 // uses heroku automated certificate management, so no need to worry about SSL stuff
 
@@ -22,35 +26,35 @@ if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone
 
 // set up session with passport & cookies
 
-const secret = process.env.SESSION_SECRET;
-app.use(
- 	session({
- 		secret: secret,
- 		resave: false,
- 		saveUninitialized: false,
- 	})
-);
-app.use(passport.initialize());
-app.use(passport.session());
+//const secret = process.env.SESSION_SECRET;
+//app.use(
+// 	session({
+// 		secret: secret,
+// 		resave: false,
+// 		saveUninitialized: false,
+// 	})
+//);
+//app.use(passport.initialize());
+//app.use(passport.session());
 
 // connect to the db and set up mongoose schemas
 
-const key = process.env.DB_SECRET;
+//const key = process.env.DB_SECRET;
 //mongoose.connect(`mongodb+srv://avynebersold:${key}@floppy-rat-database.cpcyqk5.mongodb.net/?retryWrites=true&w=majority&appName=floppy-rat-database`);
 
-const userSchema = new mongoose.Schema({
-	username: String,
- 	password: String,
- 	highscore: Number,
-});
+//const userSchema = new mongoose.Schema({
+//	username: String,
+// 	password: String,
+// 	highscore: Number,
+//});
 
-const leaderboardSchema = new mongoose.Schema({
-	scores: [{
- 		player: String,
- 		score: Number,
- 		timeCompleted: Date,
- 	}]
- });
+//const leaderboardSchema = new mongoose.Schema({
+//	scores: [{
+// 		player: String,
+// 		score: Number,
+// 		timeCompleted: Date,
+// 	}]
+// });
 
 app.get("*", (req, res, next) => {
 	if(req.protocol != "https"){

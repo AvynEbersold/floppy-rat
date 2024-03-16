9f25d36501017ebf1a975cdd3421c043c52cbb75
AVYN EBERSOLD <1793298-AVYNEBERSOLD@users.noreply.replit.com>
2024-03-16 02:28:07 +0000

    Add mongoose collections and passport init, revise HTTPS redirect. Let's see if it breaks everything.

--- server.js
+++ server.js
@@ -1,22 +1,15 @@
 var express = require("express");
-var https = require("https");
-var http = require("http");
 var fs = require("fs");
-// const mongoose = require('mongoose');
-// const session = require('express-session');
-// const passport = require('passport');
-// const passportLocalMongoose = require('passport-local-mongoose');
+const mongoose = require('mongoose');
+const session = require('express-session');
+const passport = require('passport');
+const passportLocalMongoose = require('passport-local-mongoose');
 
 const app = express();
 app.use(express.static("public"));
 app.enable('trust proxy')
 
-// use heroku automated certificate management?
-
-var options = {
-	key: fs.readFileSync("privatekey.pem", "utf8"),
-	cert: fs.readFileSync("floppyrat_com.crt", "utf8"),
-};
+// uses heroku automated certificate management, so no need to worry about SSL stuff
 
 let port = process.env.PORT || 3000;
 
@@ -28,45 +21,41 @@ if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone
   }
 }
 
-//USE COOKIES WITH ENCRYPTION KEY
-// const secret = process.env.SESSION_SECRET;
-// app.use(
-// 	session({
-// 		secret: secret,
-// 		resave: false,
-// 		saveUninitialized: false,
-// 	})
-// );
-// app.use(passport.initialize());
-// app.use(passport.session());
+// set up session with passport & cookies
 
-//CONNECT TO MONGODB DATABASE
+const secret = process.env.SESSION_SECRET;
+app.use(
+ 	session({
+ 		secret: secret,
+ 		resave: false,
+ 		saveUninitialized: false,
+ 	})
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
-// mongoose.set('useNewUrlParser', true);
-// mongoose.set('useFindAndModify', false);
-// mongoose.set('useCreateIndex', true);
-// mongoose.set('useUnifiedTopology', true);
-// const key = process.env.DB_SECRET;
-// mongoose.connect('mongodb+srv://avynebersold:' + key + '@floppy-rat-database.cpcyqk5.mongodb.net/?retryWrites=true&w=majority&appName=floppy-rat-database');
+// connect to the db and set up mongoose schemas
 
-// const userSchema = new mongoose.Schema({
-// 	username: String,
-// 	password: String,
-// 	highscore: Number,
-// });
+const key = process.env.DB_SECRET;
+mongoose.connect(`mongodb+srv://avynebersold:${key}@floppy-rat-database.cpcyqk5.mongodb.net/?retryWrites=true&w=majority&appName=floppy-rat-database`);
 
-// const leaderboardSchema = new mongoose.Schema({
-// 	scores: [{
-// 		player: String,
-// 		score: Number,
-// 		timeCompleted: Date,
-// 	}]
-// });
+const userSchema = new mongoose.Schema({
+	username: String,
+ 	password: String,
+ 	highscore: Number,
+});
+
+const leaderboardSchema = new mongoose.Schema({
+	scores: [{
+ 		player: String,
+ 		score: Number,
+ 		timeCompleted: Date,
+ 	}]
+ });
 
 app.get("*", (req, res, next) => {
 	if(req.protocol != "https"){
-		console.log(req.protocol);
-		res.redirect("https://www.floppyrat.com");
+		res.redirect('https://www.floppyrat.com' + req.originalUrl);
 	} else {
 		next();
 	}

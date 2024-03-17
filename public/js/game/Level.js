0082d3b4281dc4609a86ffc55f758cc1103cdc25
Renato Dell'Osso <54045197+renatodellosso@users.noreply.github.com>
2024-03-17 12:44:43 -0400

    Added maxPipeGapYChange and improved level 3

--- public/js/game/Level.js
+++ public/js/game/Level.js
@@ -1,9 +1,17 @@
 class Level {
-    constructor(pipeCount, pipeSpacing, gapSize, gapRange, nextLevel = null) {
+    constructor(
+        pipeCount,
+        pipeSpacing,
+        gapSize,
+        gapRange,
+        maxGapYChange,
+        nextLevel = null
+    ) {
         this.pipeCount = pipeCount;
         this.pipeSpacing = pipeSpacing;
         this.pipeGap = gapSize;
         this.gapRange = gapRange;
+        this.maxGapYChange = maxGapYChange;
         this.nextLevel = nextLevel;
     }
 
@@ -24,6 +32,7 @@ class Level {
         }
 
         let pipeGapY = generatePipeGapY(this);
+        let prevPipeGapY = pipeGapY;
         let pipeX = 1.2;
 
         for (let i = 0; i < this.pipeCount; i++) {
@@ -31,7 +40,22 @@ class Level {
             game.addGameObject(pipe);
 
             pipeX += this.pipeSpacing;
+
+            prevPipeGapY = pipeGapY;
             pipeGapY = generatePipeGapY(this);
+
+            // Ensure that the gap between pipes doesn't change too much
+            if (pipeGapY - prevPipeGapY > this.maxGapYChange) {
+                pipeGapY = prevPipeGapY - this.maxGapYChange;
+            }
+
+            if (pipeGapY - prevPipeGapY < -this.maxGapYChange) {
+                pipeGapY = prevPipeGapY + this.maxGapYChange;
+            }
+
+            // Ensure that the gap between pipes doesn't go out of bounds
+            pipeGapY = Math.min(pipeGapY, 1 - this.pipeGap);
+            pipeGapY = Math.max(pipeGapY, this.pipeGap);
 
             if (i == this.pipeCount - 1) {
                 pipe.withPassedByPlayerCallback(this.swapToNextLevel);
@@ -47,7 +71,14 @@ class Level {
 }
 
 class LevelList {
-    static level3 = new Level(5, 0.15, 0.4, [0.065, 0.65]);
-    static level2 = new Level(5, 0.4, 0.275, [0.075, 0.6]);
-    static level1 = new Level(5, 0.5, 0.3, [0.1, 0.5], LevelList.level2);
+    static level3 = new Level(5, 0.25, 0.4, [0.1, 0.5], 0.2);
+    static level2 = new Level(
+        5,
+        0.4,
+        0.275,
+        [0.075, 0.6],
+        0.4,
+        LevelList.level3
+    );
+    static level1 = new Level(5, 0.5, 0.3, [0.1, 0.5], 0.4, LevelList.level2);
 }

0deb83556fa245b8da4be793cffe4d5f5ece6844
Renato Dell'Osso <54045197+renatodellosso@users.noreply.github.com>
2024-03-19 09:54:58 -0400

    Readded 1st 3 levels

--- public/js/game/Level.js
+++ public/js/game/Level.js
@@ -73,7 +73,7 @@ class Level {
 
     swapToNextLevel() {
         this.end();
-        
+
         console.log("Swapping to next level...");
         game.setLevel(Math.min(game.levelIndex + 1, levelList.length - 1));
         game.level.start();
@@ -91,9 +91,9 @@ class Level {
 }
 
 const levelList = [
-    // new Level(5, 0.5, 0.3, [0.1, 0.3], 0.4),
-    // new Level(5, 0.4, 0.275, [0.075, 0.325], 0.4),
-    // new Level(5, 0.25, 0.4, [0.0, 0.3], 0.2),
+    new Level(5, 0.5, 0.3, [0.1, 0.3], 0.4),
+    new Level(5, 0.4, 0.275, [0.075, 0.325], 0.4),
+    new Level(5, 0.25, 0.4, [0.0, 0.3], 0.2),
     new Level(5, 0.5, 0.3, [0.1, 0.3], 0.4)
         .withStartCallback((level) => {
             config.gravity *= -1;

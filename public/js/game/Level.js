41c6b48362d11eb2891bb8f5633b440aeb411a36
FireLemon543910 <101438032+kmangodawgs@users.noreply.github.com>
2024-03-20 16:37:07 +0000

    Add comments showing level init arguments

--- public/js/game/Level.js
+++ public/js/game/Level.js
@@ -91,6 +91,7 @@ class Level {
 }
 
 const levelList = [
+  //(pipeCount, pipeSpacing, gapSize, gapRange, maxGapYChange)
     new Level(5, 0.5, 0.3, [0.1, 0.3], 0.4),
     new Level(5, 0.4, 0.275, [0.075, 0.325], 0.4),
     new Level(5, 0.25, 0.4, [0.0, 0.3], 0.2),

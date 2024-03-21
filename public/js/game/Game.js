851e8f21da47b9aa2d494ec5290f5007306ab1dd
AvynEbersold <48456012+AvynEbersold@users.noreply.github.com>
2024-03-21 14:55:55 +0000

    Remove score counter (at top right) when in the menu

--- public/js/game/Game.js
+++ public/js/game/Game.js
@@ -1,144 +1,143 @@
 class Game {
-    gameObjects = [];
-    lastTimeMs = 0;
-    deltaTimeMs = 0;
-    globalGameLoop = 0;
-    mousePos = new Vector(0, 0);
+	gameObjects = [];
+	lastTimeMs = 0;
+	deltaTimeMs = 0;
+	globalGameLoop = 0;
+	mousePos = new Vector(0, 0);
 
-    score = 0;
-    highScore = localStorage.getItem("highScore") || 0;
+	score = 0;
+	highScore = localStorage.getItem("highScore") || 0;
 
-    gameplayStarted = false;
-    gameplayStartedCallbacks = [];
+	gameplayStarted = false;
+	gameplayActive = false;
+	gameplayStartedCallbacks = [];
 
-    constructor() {
-        this.canvas = initCanvas();
-        this.lastTimeMs = Date.now();
-    }
+	constructor() {
+		this.canvas = initCanvas();
+		this.lastTimeMs = Date.now();
+	}
 
-    loop() {
-        this.deltaTimeMs = Date.now() - this.lastTimeMs;
-        this.lastTimeMs = Date.now();
+	loop() {
+		this.deltaTimeMs = Date.now() - this.lastTimeMs;
+		this.lastTimeMs = Date.now();
 
-        this.canvas.clearRect(0, 0, window.innerWidth, window.innerHeight);
+		this.canvas.clearRect(0, 0, window.innerWidth, window.innerHeight);
 
-        this.render(RenderStep.Early);
-        this.update();
-        this.render(RenderStep.Main);
-        this.render(RenderStep.Late);
+		this.render(RenderStep.Early);
+		this.update();
+		this.render(RenderStep.Main);
+		this.render(RenderStep.Late);
 
-        requestAnimationFrame(this.loop.bind(this));
-    }
+		requestAnimationFrame(this.loop.bind(this));
+	}
 
-    update() {
-        this.gameObjects.forEach((gameObject) => {
-            gameObject.update(this.deltaTimeMs / 1000);
-        });
-    }
+	update() {
+		this.gameObjects.forEach((gameObject) => {
+			gameObject.update(this.deltaTimeMs / 1000);
+		});
+	}
 
-    render(renderStep) {
-        this.gameObjects
-            .filter((o) => o.renderStep === renderStep)
-            .forEach((gameObject) => {
-                gameObject.render(this.canvas);
-            });
-    }
+	render(renderStep) {
+		this.gameObjects
+			.filter((o) => o.renderStep === renderStep)
+			.forEach((gameObject) => {
+				gameObject.render(this.canvas);
+			});
+	}
 
-    keyDown(key) {
-        if (!this.gameplayStarted)
-            this.level.start();
-        this.gameplayStarted = true;
-        
-        this.gameObjects.forEach((gameObject) => {
-            gameObject.keyDown(key);
-        });
-    }
+	keyDown(key) {
+		if (!this.gameplayStarted) this.level.start();
+		this.gameplayStarted = true;
 
-    onClick() {
-        if (!this.gameplayStarted)
-            this.level.start();
-        this.gameplayStarted = true;
+		this.gameObjects.forEach((gameObject) => {
+			gameObject.keyDown(key);
+		});
+	}
 
-        this.gameObjects.forEach((gameObject) => {
-            gameObject.onClick();
-        });
-    }
+	onClick() {
+		if (!this.gameplayStarted) this.level.start();
+		this.gameplayStarted = true;
 
-    onMouseMove(pos) {
-        this.mousePos = new Vector(
-            pos.x / dimensions.width,
-            pos.y / dimensions.width
-        );
-    }
+		this.gameObjects.forEach((gameObject) => {
+			gameObject.onClick();
+		});
+	}
 
-    addGameObject(gameObject) {
-        if (gameObject instanceof GameObject)
-            this.gameObjects.push(gameObject);
-    }
+	onMouseMove(pos) {
+		this.mousePos = new Vector(
+			pos.x / dimensions.width,
+			pos.y / dimensions.width,
+		);
+	}
 
-    removeGameObject(gameObject) {
-        this.gameObjects = this.gameObjects.filter((obj) => obj !== gameObject);
-    }
+	addGameObject(gameObject) {
+		if (gameObject instanceof GameObject) this.gameObjects.push(gameObject);
+	}
 
-    start() {
-        console.log("Starting game...");
-        game.globalGameLoop++;
-        console.log("Game Loop:" + game.globalGameLoop);
-        keyObjects.player = new PlayerObject();
+	removeGameObject(gameObject) {
+		this.gameObjects = this.gameObjects.filter((obj) => obj !== gameObject);
+	}
 
-        for (const step in RenderStep) {
-            console.log(`Rendering step: ${step}`);
-            this.render(step);
-        }
+	start() {
+		console.log("Starting game...");
+		game.globalGameLoop++;
+		console.log("Game Loop:" + game.globalGameLoop);
+		keyObjects.player = new PlayerObject();
+		keyObjects.scoreCounter.withRenderStep(RenderStep.Late);
 
-        if (this.gameplayStarted) {
-            for (const callback of this.gameplayStartedCallbacks) {
-                callback();
-            }
+		for (const step in RenderStep) {
+			console.log(`Rendering step: ${step}`);
+			this.render(step);
+		}
 
-            this.level.start();
-        }
-    }
+		if (this.gameplayStarted) {
+			for (const callback of this.gameplayStartedCallbacks) {
+				callback();
+			}
 
-    startLoop() {
-        console.log("Starting game loop...");
-        // // Not sure why we can't just pass Game.instance.loop directly to setInterval
-        // setInterval(() => game.loop(), 1000 / config.fps);
-        requestAnimationFrame(this.loop.bind(this));
+			this.level.start();
+		}
+	}
 
-        for (const callback of this.gameplayStartedCallbacks) {
-            callback();
-        }
+	startLoop() {
+		console.log("Starting game loop...");
+		// // Not sure why we can't just pass Game.instance.loop directly to setInterval
+		// setInterval(() => game.loop(), 1000 / config.fps);
+		requestAnimationFrame(this.loop.bind(this));
 
-        if (this.gameplayStarted)
-            this.level.start();
-    }
+		for (const callback of this.gameplayStartedCallbacks) {
+			callback();
+		}
 
-    end() {
-        console.log("Ending game...");
+		if (this.gameplayStarted && this.isActive) this.level.start();
+	}
 
-        if (this.score > this.highScore) {
-            this.highScore = this.score;
-            localStorage.setItem("highScore", this.highScore);
-        }
+	end() {
+		console.log("Ending game...");
 
-        const fader = createFaderObject();
-        const scoreText = createScoreText();
-        const highScoreText = createHighScoreText();
-        createPlayButtonObject(fader, scoreText, highScoreText);
+		if (this.score > this.highScore) {
+			this.highScore = this.score;
+			localStorage.setItem("highScore", this.highScore);
+		}
 
-        this.gameplayStartedCallbacks = [];
+		const fader = createFaderObject();
+		const scoreText = createScoreText();
+		const highScoreText = createHighScoreText();
+		createPlayButtonObject(fader, scoreText, highScoreText);
+		keyObjects.scoreCounter.withRenderStep("none");
 
-        this.setLevel(0);
-        this.score = 0;
-    }
+		this.gameplayStartedCallbacks = [];
 
-    addGameplayStartedCallback(callback) {
-        this.gameplayStartedCallbacks.push(callback);
-    }
+		this.setLevel(0);
+		this.score = 0;
+	}
 
-    setLevel(index) {
-        this.levelIndex = index;
-        this.level = levelList[index];
-    }
+	addGameplayStartedCallback(callback) {
+		this.gameplayStartedCallbacks.push(callback);
+	}
+
+	setLevel(index) {
+		this.levelIndex = index;
+		this.level = levelList[index];
+	}
 }
--- public/js/game/gameobjects/MiscObjects.js
+++ public/js/game/gameobjects/MiscObjects.js
@@ -76,7 +76,7 @@ function createPlayButtonObject(fader, scoreText, highScoreText) {
 
     return new ImageObject(
         "images/FB_Start.png",
-        new Rect(0.4, (dimensions.heightToWidthRatio - 0.2) / 2, 0.2, 0.2)
+        new Rect(0.375, dimensions.heightToWidthRatio * 0.325, 0.25, 0.25)
     )
         .withUpdateCallback((obj, deltaTime) => {
             obj.color.a = obj.overlaps(game.mousePos) ? 0.5 : 1;
@@ -88,21 +88,21 @@ function createPlayButtonObject(fader, scoreText, highScoreText) {
 }
 
 function createScoreText() {
-    return new TextObject(0.5, 0.55, `score: ${game.score}`, {
-        size: 40,
+    return new TextObject(0.5, 0.57, `score: ${game.score}`, {
+        size: 50,
         align: "center",
     });
 }
 
 function createHighScoreText() {
-    return new TextObject(0.5, 0.6, `high score: ${game.highScore}`, {
+    return new TextObject(0.5, 0.63, `high score: ${game.highScore}`, {
         size: 40,
         align: "center",
     });
 }
 
 function createScoreCounter() {
-    new TextObject(0.97, 0.05, "0", { size: 35, align: "right" })
+    return new TextObject(0.97, 0.05, "0", { size: 35, align: "right" })
         .withUpdateCallback((obj, deltaTime) => {
             obj.text = `${game.score} / ${game.levelIndex + 1}`;
         })
